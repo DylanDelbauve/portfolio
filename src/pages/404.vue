345ca8bbd816e0857a215233f2cc97642f65058e
DylanDelbauve <dylandelbauve@gmail.com>
2022-03-04 15:30:40 +0100

    404

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>oops</h1>
+  <div class="w-screen h-screen relative bg-gray-800 flex flex-col justify-center items-center">
+    <h1 class="text-white animate-pulse static text-[200px] md:text-[400px] font-mono ordinal h-fit">404</h1>
+    <h2 class="text-gray-400 relative -top-20">La page que vous recherchez n'existe pas</h2>
+  </div>
 </template>
 
 <script>
