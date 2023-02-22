17265afa257b42b84afdd5b5326c1e9583814db2
Dylan D <dylandelbauve@gmail.com>
2023-02-22 19:29:28 +0100

    Nitro output

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  "nitro": {
+    "output": {
+      dir: 'output',
+      serverDir: 'output/server',
+      publicDir: 'output/public'
+    }
+  },
   modules: [
     "@nuxt/content",
     "nuxt-icon",
