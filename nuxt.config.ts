d118ef4ab6a2dfaa2d6fa8682ee491b554401f2c
Dylan D <dylandelbauve@gmail.com>
2023-02-24 22:56:39 +0100

    Revert "Nitro output"
    This reverts commit 17265afa257b42b84afdd5b5326c1e9583814db2.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  "nitro": {
-    "output": {
-      dir: 'output',
-      serverDir: 'output/server',
-      publicDir: 'output/public'
-    }
-  },
   modules: [
     "@nuxt/content",
     "nuxt-icon",
